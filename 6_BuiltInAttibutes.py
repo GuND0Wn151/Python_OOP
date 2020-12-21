f981c6bb7f6d1ddb6af80bbdfc70d64deb585bbe
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-21 16:58:56 +0530

    Update 6_BuiltInAttibutes.py

--- 6_BuiltInAttibutes.py
+++ 6_BuiltInAttibutes.py
@@ -1,3 +1,7 @@
+#C0d3 n0=6
+#made By GuND0Wn151
+
+
 class Valorant:
       """
       Valorant class this class has attributes as health agent gun
@@ -28,9 +32,20 @@ player1.GetReady()
 player1.BuyGun()
 player1.display()
 
+#__doc__ gives the docmentation in the class if nthg is meantioned then it returns non
 print(player1.__doc__,"\n")
+
+#__name__ return name of the class
 print(Valorant.__name__,"\n")
+
+#this attributes returns a dictonary whihc has the all attributes of the class mentioned and also with their address
+# and also the adress of variables used in the class
 print(Valorant.__dict__,"\n")
+
+# possibly empty tuple containing the base classes, in the order of their occurrence in the base class list.
 print(Valorant.__bases__,"\n")
+
+# Module name in which the class is defined. This attribute is "__main__" in interactive mode.
 print(player1.__module__)
 
+#this are the some useful builtin attributes of python classes and objects
